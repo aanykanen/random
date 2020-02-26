21d4389f756c5f95a244ab8d7401086e3ba3ec0f
Aatu Nykänen <aatu.nykanen@outlook.com>
2020-02-27 01:50:50 +0200

    one does not simply obfuscate code

---
+++ obfuscation_using_memes/README.md
@@ -0,0 +1,4 @@
+# Obfuscating c++ code using memes
+
+## Origin
+Advanced object oriented programming course had an assignment where one was required to develop operator << using function templates. I could not resist making a few additional #defines..
---
+++ obfuscation_using_memes/uniq_ptr_output.h
@@ -0,0 +1,48 @@
+#define tool (
+#define meme <iostream>
+#define sharpest operator<<
+#define _the return
+#define in_the std
+#define me template
+#define she os
+#define forehead }
+#define roll typename
+#define her_ }
+#define somebody namespace
+#define shape os
+#define of T
+#define of_ <<
+#define I >
+#define shed ostream
+#define me_ U
+#define on ;
+#define her_thumb )
+#define _in {
+#define with U
+#define her >
+#define looking unique_ptr
+#define is T
+#define _and ptr
+#define kind <
+#define gonna ,
+#define finger const
+#define was ,
+#define world typename
+#define an_L _and
+#define the <
+#define aint std
+#define music <memory>
+#define once std
+#define dumb ,
+#define told {
+#define the_ ostream
+
+// The difference between stupidity and genius is that genius has its limits
+
+#include meme
+#include music
+
+somebody once told me the world is gonna roll me_
+I aint::the_& sharpest tool in_the::shed& 
+she was looking kind of dumb with her finger& _and her_thumb
+_in _the shape of_ *an_L on her_ forehead
